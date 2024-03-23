59ecab838a4c72a5ce4936b55587a3ee8d99871b
mezeirobert0 <antoniu.mezei@stud.ubbcluj.ro>
2024-03-23 14:30:33 +0200

    Add project files.

---
+++ App.cpp
@@ -0,0 +1,16 @@
+#include "Set.h"
+#include "SetIterator.h"
+#include "ExtendedTest.h"
+#include "ShortTest.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	testAll();
+	testAllExtended();
+
+	cout << "That's all!" << endl;
+	system("pause");
+
+}
